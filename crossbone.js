87ef36c1ab9c1cf9bd0b1f50f5a63f9e304974da
Jivings <james@ivings.org.uk>
2013-04-20 19:24:24 +0100

    Example page and base chart view/model

--- crossbone.js
+++ crossbone.js
@@ -8,9 +8,15 @@
     localStorage: new Backbone.LocalStorage("crossbone-charts"),
   });
 
-  
-
   this.CrossBone = Backbone.Model.extend({
+
+    constructor: function() {
+      var attributes = arguments[1] || {};
+      if ( arguments.length > 0 ) {
+        attributes.data = arguments[0];
+      }
+      Backbone.Model.apply(this, [ attributes, Array.prototype.slice.call(arguments, 1)]);
+    },
 
     initialize: function() {
       this.crossfilter = new crossfilter(this.attributes.data);
@@ -82,6 +88,12 @@
     constructor: function() {
       arguments[1].id = arguments[0];
       Backbone.Model.apply(this, Array.prototype.slice.call(arguments, 1));
+    },
+
+    render: function() {
+      this.$el.find('.title').text(this.model.get('title'))
+        .height(this.model.get('height'))
+        .width(this.model.get('width'));
     }
   });
 
@@ -92,17 +104,31 @@
     }
   });
 
+  /*
+   * Data Chart
+   */
   var DataChartView = BaseChartView.extend({
+    
     render: function() {
-      this.$el.find('.filtered').text(this.model.get('filtered'));
-      this.$el.find('.total').text(this.model.get('total'));
+      var $filtered = this.model.get('filteredRecords');
+      var $total = this.model.get('totalRecords');
+
+      this.$el.find($filtered)
+        .text(this.model.get('filtered'));
+
+      this.$el.find($total)
+        .text(this.model.get('total'));
       return this;
     }
   });
 
   var DataChart = BaseChart.extend({
     defaults: _.extend({}, BaseChart.prototype.defaults(), { 
-      title : 'Data' 
+      title : 'Data',
+      filteredRecords: '.filtered',
+      totalRecords: '.total',
+      filtered: 0,
+      total: 0
     }),
 
     initialize: function() {
@@ -110,6 +136,59 @@
         .set('total', this.attributes.dimension.size())
         .set('filtered', this.attributes.group.value());
     }
+  });
+
+  /*
+   *  Pie Chart
+   */ 
+  var PieChartView = BaseChartView.extend({
+    render: function() {
+      var width = this.model.get('width'),
+          height = this.model.get('height'),
+          radius = Math.min(width, height) / 2;
+
+      var color = this.model.get('color');
+      
+
+      var arc = d3.svg.arc()
+          .outerRadius(radius - 10)
+          .innerRadius(0);
+
+      var pie = d3.layout.pie()
+          .sort(null)
+          .value(function(d) { return this.model.group().size(); }) ;
+
+      var svg = d3.select("body").append("svg")
+          .attr("width", width)
+          .attr("height", height)
+        .append("g")
+          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+      var pieData = this.model.group().naturalOrder().top(Infinity)
+
+        var g = svg.selectAll(".arc")
+            .data(pie(pieData))
+          .enter().append("g")
+            .attr("class", "arc");
+
+        g.append("path")
+            .attr("d", arc)
+
+        g.append("text")
+            .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+            .attr("dy", ".35em")
+            .style("text-anchor", "middle")
+
+    }
+  });
+
+  var PieChart = BaseChart.extend({
+    defaults: _.extend({}, BaseChart.prototype.defaults(), { 
+      title : 'Data',
+      color : d3.scale.ordinal()
+                .range(["#98abc5", "#8a89a6", "#7b6888", 
+                  "#6b486b", "#a05d56", "#d0743c", "#ff8c00"])
+    }),
   });
   
   
